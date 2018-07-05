08de2858fa1b0bc7ced0cea12baa85482098d98a
Peter Nielson <computergeekpete@gmail.com>
2018-07-05 11:50:02 -0600

    Increased number of iterations

--- travis.sh
+++ travis.sh
@@ -7,7 +7,7 @@ echo `g++ -Wall -pthread -lpthread Race.cc`
 echo
 echo
 echo "Running valgrind on Race.cc with 1000000 as argument"
-echo `valgrind --leak-check=full --show-leak-kinds=all  ./a.out "1000000"`
+echo `valgrind --leak-check=full --show-leak-kinds=all  ./a.out "100000000"`
 echo
 echo
 echo "Running clang-tidy on Race.cc"
@@ -15,7 +15,7 @@ echo `clang-tidy Race.cc -- -Imy_project/include -DMY_DEFINES ...`
 echo
 echo
 echo
-echo "Running ./a.out '1000000'"
-./a.out 1000000
+echo "Running ./a.out '100000000'"
+./a.out 100000000
 
 exit 0
